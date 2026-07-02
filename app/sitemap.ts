4dbc76d5841e5aec1c1547cef68add84829c11ef
Lucier Lima <lucierflima@gmail.com>
2026-07-02 16:12:34 -0300

    configuração sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://lucierlima.dev',
+      url: 'https://lucier-portifolio.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
