27c5ea1c024890d0983a531ec5424a72ec076732
Lucier Lima <lucierflima@gmail.com>
2026-07-02 16:17:57 -0300

    configuração sitemap.xml

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://lucierlima.dev/sitemap.xml',
+    sitemap: 'https://lucier-portifolio.vercel.app/sitemap.xml',
   }
 }
